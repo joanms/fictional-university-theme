7dcfa9e225861949fe08f8b90ea7a526737c79f8
Joan Sweeney <joanms@eircom.net>
2020-09-21 16:17:00 +0100

    Fix bug preventing theme from using styles

--- js/scripts.js
+++ js/scripts.js
@@ -9,6 +9,7 @@ import HeroSlider from "./modules/HeroSlider"
 var mobileMenu = new MobileMenu()
 var heroSlider = new HeroSlider()
 
+
 // Allow new JS and CSS to load in browser without a traditional page refresh
 if (module.hot) {
   module.hot.accept()
